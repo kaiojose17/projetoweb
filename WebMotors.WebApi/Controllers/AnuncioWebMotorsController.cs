260dbb5d1f8cc0385f533073cb658429da6a1781
jose.silva <jose.silva@accesscorp.com.br>
2019-04-12 13:38:33 -0300

    Criação da Controller.

---
+++ WebMotors.WebApi/Controllers/AnuncioWebMotorsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMotors.Application.Interface;
+
+namespace WebMotors.WebApi.Controllers
+{
+    public class AnuncioWebMotorsController : Controller
+    {
+        private readonly IWebMotorsServiceBase _webMotorsServiceBase;
+
+        public AnuncioWebMotorsController(IWebMotorsServiceBase webMotorsServiceBase)
+        {
+            _webMotorsServiceBase = webMotorsServiceBase;
+        }
+
+        // GET: AnuncioWebMotors
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
